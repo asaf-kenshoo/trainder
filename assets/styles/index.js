1436f93d2a22e4ad0a0c79d2e18354722363cbc5
asaf-kenshoo <asaf.levy@kenshoo.com>
2021-02-16 21:51:52 +0200

    fixing profile

--- assets/styles/index.js
+++ assets/styles/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from "react-native";
+import {StyleSheet, Dimensions} from "react-native";
 
 const PRIMARY_COLOR = "#279A72";
 const SECONDARY_COLOR = "#279A72";
@@ -126,7 +126,7 @@ export default StyleSheet.create({
         flex: 1,
     },
     headerLogo: {
-        width:130,
+        width: 130,
         flexDirection: "row",
         height: '100%',
         alignItems: 'center',
@@ -135,8 +135,8 @@ export default StyleSheet.create({
     headerText: {
         // fontFamily: ICON_FONT,
         color: WHITE,
-        paddingTop:30,
-        paddingLeft:30,
+        paddingTop: 30,
+        paddingLeft: 30,
         fontSize: 34
     },
 
@@ -162,76 +162,76 @@ export default StyleSheet.create({
         paddingTop: 5
     },
 
-	// COMPONENT - PROFILE ITEM
-  centeralizeProfileContent: {
-    justifyContent: 'top',
-    alignItems: 'center'
-  },
-	containerProfileItem: {
-		backgroundColor: WHITE,
-		paddingHorizontal: 10,
-		paddingBottom: 25,
-		margin: 20,
-		borderRadius: 8,
-		shadowOpacity: 0.05,
-		shadowRadius: 10,
-		shadowColor: BLACK,
-		shadowOffset: { height: 0, width: 0 }
-	},
-	profileImage:{
-			width: 100,
-			height: 100,
-			borderRadius: 100 / 2,
-			backgroundColor: "red",
-	},
-	matchesProfileItem: {
-		marginTop: 15,
-		paddingVertical: 7,
-		paddingHorizontal: 20,
-		borderRadius: 20,
-		textAlign: "center",
-		justifyContent:"center",
-		alignSelf: "center"
-	},
-  profileInviteFriendsLabel: {
-    backgroundColor: '#32a897',
-    height: 35,
-    width: 130,
-    marginLeft: 5,
-    borderRadius : 15
-  },
-	matchesTextProfileItem: {
-		// fontFamily: ICON_FONT,
-		color: WHITE
-	},
-	name: {
-		paddingTop: 2,
-		color: DARK_GRAY,
-		fontSize: 15,
-		textAlign: "center"
-	},
-	descriptionProfileItem: {
-		color: GRAY,
-		textAlign: "center",
-		paddingBottom: 20,
-		fontSize: 13
-	},
-	info: {
-		flexDirection: "row",
-		alignItems: "center",
-    backgroundColor: '#ebf5ee',
-    borderRadius: 4
-	},
-	iconProfile: {
-		// fontFamily: ICON_FONT,
-		fontSize: 12,
-		color: DARK_GRAY,
-		paddingHorizontal: 10
-	},
-	infoContent: {
-		color: GRAY,
-		fontSize: 13
-	},
+    // COMPONENT - PROFILE ITEM
+    centeralizeProfileContent: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    containerProfileItem: {
+        backgroundColor: WHITE,
+        paddingHorizontal: 10,
+        paddingBottom: 25,
+        margin: 20,
+        borderRadius: 8,
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        shadowColor: BLACK,
+        shadowOffset: {height: 0, width: 0}
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+        backgroundColor: "red",
+    },
+    matchesProfileItem: {
+        marginTop: 15,
+        paddingVertical: 7,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        textAlign: "center",
+        justifyContent: "center",
+        alignSelf: "center"
+    },
+    profileInviteFriendsLabel: {
+        backgroundColor: '#32a897',
+        height: 35,
+        width: 130,
+        marginLeft: 5,
+        borderRadius: 15
+    },
+    matchesTextProfileItem: {
+        // fontFamily: ICON_FONT,
+        color: WHITE
+    },
+    name: {
+        paddingTop: 2,
+        color: DARK_GRAY,
+        fontSize: 15,
+        textAlign: "center"
+    },
+    descriptionProfileItem: {
+        color: GRAY,
+        textAlign: "center",
+        paddingBottom: 20,
+        fontSize: 13
+    },
+    info: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: '#ebf5ee',
+        borderRadius: 4
+    },
+    iconProfile: {
+        // fontFamily: ICON_FONT,
+        fontSize: 12,
+        color: DARK_GRAY,
+        paddingHorizontal: 10
+    },
+    infoContent: {
+        color: GRAY,
+        fontSize: 13
+    },
 
     // CONTAINER - GENERAL
     bg: {
--- containers/Profile.js
+++ containers/Profile.js
@@ -2,32 +2,35 @@ import React from 'react';
 import styles from '../assets/styles';
 
 import {
-  ScrollView,
-  View,
-  Text,
-  ImageBackground,
-  TouchableOpacity
+    ScrollView,
+    View,
+    Text,
+    ImageBackground,
+    TouchableOpacity
 } from 'react-native';
 import ProfileItem from '../components/ProfileItem';
+
 const Profile = () => {
 
-  return (
+    return (
 
-      <ScrollView style={styles.centeralizeProfileContent}>
-        <ProfileItem style={{PaddingTop: "-60%"}}
-          name={"Asaf The Snitch"}
-          iconPath={"https://i.ibb.co/ChM7Fkq/image.png"}
-          score={234}
-          level={1}
-          numberOfAnswers={123}
-        />
-      <View style={styles.centeralizeProfileContent}>
-        <View style={styles.profileInviteFriendsLabel}>
-          <Text style={{ color: 'white', textAlign: 'center', paddingTop: 8}}>INVITE FRIENDS </Text>
-        </View>
-      </View>
-      </ScrollView>
-  );
+        <ScrollView>
+            <View style={styles.centeralizeProfileContent}>
+                <ProfileItem style={{PaddingTop: "-60%"}}
+                             name={"New User !!!"}
+                             iconPath={"https://i.ibb.co/ChM7Fkq/image.png"}
+                             score={234}
+                             level={1}
+                             numberOfAnswers={123}
+                />
+                <View >
+                    <View style={styles.profileInviteFriendsLabel}>
+                        <Text style={{color: 'white', textAlign: 'center', paddingTop: 8}}>INVITE FRIENDS </Text>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
+    );
 };
 
 export default Profile;
